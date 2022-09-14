ca3432f326062de1e2987bc0a2c73795ff651e70
madhavimaddula <113557661+madhavimaddula@users.noreply.github.com>
2022-09-14 15:10:46 -0700

    Create Main.java

---
+++ src/com/madhavi/Main.java
@@ -0,0 +1,10 @@
+package com.madhavi;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Implemented unit tests simpy with sys out as we don't have JUnit dependencies.
+        TDDTester tester = new TDDTester();
+        tester.runTests();
+    }
+}
